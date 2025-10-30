d02a5ce4e8bfb694efe0d7ffa64ccca2395ee451
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-30 13:35:43 +0600

    Check if all strings in an array have length greater than 3

--- every.js
+++ every.js
@@ -2,3 +2,8 @@
 const numbers = [2, 5, 8, 10];
 const positive = numbers.every(num => num > 0)
 console.log(positive);
+
+//Check if all strings in an array have length greater than 3
+const words = ["JavaScript", "React", "Node", "CSS"];
+const longStrings = words.every(word => word > 3)
+console.log(longStrings);
