1c8d9d746e2dcbf5d1cdb23ecd03070f7b63e7f9
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-30 11:47:36 +0600

    find the first even number problem using finde method done

--- findIndex.js
+++ findIndex.js
@@ -2,3 +2,21 @@ const fruits = ["apple", "banana", "cherry", "mango"];
 const fruit = (element) => element === "cherry"
 const position = fruits.findIndex(fruit);
 console.log(position);
+
+// Object array-এ find(id == 2)
+
+const users = [
+    { id: 1, name: "Shantunu" },
+    { id: 2, name: "Ersa" },
+    { id: 3, name: "Rakib" }
+];
+
+const foundUser = users.find(user => user.id === parseInt("1")) // this is better because it's code predictable, dynamic input, user-driven data
+// const foundUser = users.find(user => user.id === 2)
+console.log(foundUser);
+
+//Find First Even Number
+const numbers = [1, 3, 7, 8, 10, 13];
+
+const firstEven = numbers.find(num => num % 2 === 0)
+console.log(firstEven);
