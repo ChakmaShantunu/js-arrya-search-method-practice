4e9bd97baea6fef3a20823f9dcf9831d14325f71
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-30 13:21:34 +0600

    logn string check with some method done

--- some.js
+++ some.js
@@ -2,3 +2,11 @@
 const numbers = [3, 7, -2, 8, 10];
 const negativeNum = numbers.some(num => num < 0);
 console.log(negativeNum);
+
+//Long String Check
+const pets = ["cat", "elephant", "dog", "rat"];
+const longPet = pets.some(pet => pet.length > 3)
+console.log(longPet);
+
+
+
