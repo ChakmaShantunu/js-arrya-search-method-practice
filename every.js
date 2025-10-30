b5037c8301a077c3fb561b3ea32886d97d186792
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-30 13:37:24 +0600

    Check if all students have marks greater than 50

--- every.js
+++ every.js
@@ -7,3 +7,12 @@ console.log(positive);
 const words = ["JavaScript", "React", "Node", "CSS"];
 const longStrings = words.every(word => word > 3)
 console.log(longStrings);
+
+//Check if all students have marks greater than 50
+const students = [
+    { name: "Rafi", marks: 75 },
+    { name: "Nila", marks: 82 },
+    { name: "Sajid", marks: 68 }
+];
+const goodMarks = students.every(student => student.marks > 50)
+console.log(goodMarks);
